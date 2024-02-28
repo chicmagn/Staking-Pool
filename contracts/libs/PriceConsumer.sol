1e786d39ad12d8988405df8064e3d36e523010ac
Epitome <defibizzylos@gmail.com>
2024-02-28 21:45:39 +0800

    Added Consumer Contract

---
+++ contracts/libs/PriceConsumer.sol
@@ -0,0 +1,52 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.24;
+
+import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+
+contract PriceConsumerV3 {
+    AggregatorV3Interface internal priceFeed;
+
+    /**
+     * Network: Ethereum Goerli
+     * Aggregator: ETH/USD
+     * Address: 0xD4a33860578De61DBAbDc8BFdb98FD742fA7028e
+     */
+    constructor(address _priceFeedAddress) {
+        priceFeed = AggregatorV3Interface(_priceFeedAddress);
+    }
+
+    /**
+     * Returns the latest price.
+     */
+    function getLatestPrice() public view returns (int, uint80) {
+        // prettier-ignore
+        (
+            uint80 roundID,
+            int price,
+            /*uint startedAt*/,
+            /*uint timeStamp*/,
+            /*uint80 answeredInRound*/
+        ) = priceFeed.latestRoundData();
+        return (price, roundID);
+    }
+
+    /**
+     * Returns historical price for a round id.
+     * roundId is NOT incremental. Not all roundIds are valid.
+     * You must know a valid roundId before consuming historical data.
+     *
+     * @dev A timestamp with zero value means the round is not complete and should not be used.
+     */
+    function getHistoricalPrice(uint80 _roundId) public view returns (int256) {
+        // prettier-ignore
+        (
+            /*uint80 roundID*/,
+            int price,
+            /*uint startedAt*/,
+            uint timeStamp,
+            /*uint80 answeredInRound*/
+        ) = priceFeed.getRoundData(_roundId);
+        require(timeStamp > 0, "Round not complete");
+        return price;
+    }
+}
--- contracts/libs/PriceConverter.sol
+++ contracts/libs/PriceConverter.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.8.24;
 
 import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
-
+import "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
 
 library PriceConverter {
 
